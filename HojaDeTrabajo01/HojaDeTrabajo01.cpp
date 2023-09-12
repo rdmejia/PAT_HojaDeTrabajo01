c4d86223d51f3738a8bede8da569d4351b31bbb6
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-09-11 21:28:00 -0600

    Update HojaDeTrabajo01.cpp
    hoja 1, nombre y carnet

--- HojaDeTrabajo01/HojaDeTrabajo01.cpp
+++ HojaDeTrabajo01/HojaDeTrabajo01.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Yasmely Dubraska Fariñas Aldana \n  1329223";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
