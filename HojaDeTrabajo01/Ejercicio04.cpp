6b3aa7af5f51c0dd37e669a5cdaa427fdff5782f
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-09-11 22:47:48 -0600

    Update Ejercicio04.cpp
    ejercicio 4, listo

--- HojaDeTrabajo01/Ejercicio04.cpp
+++ HojaDeTrabajo01/Ejercicio04.cpp
@@ -16,3 +16,24 @@ vector<string> Ejercicio04::generateParenthesis(int n, string Ct, int A, int C, 
         Ct (n, A, C + 1, C, Ct + resultado);
     }
 }
+
+vector<string> generarCombinacionesDeParentesis(int n)
+{
+    vector<string> resultados;
+    generarCombinaciones(n, 0, 0, "", resultados);
+    return resultados;
+}
+
+int main() {
+    int n;
+    cout << "Ingrese el número de pares de paréntesis: ";cin >> n;
+
+    vector<string> combinaciones = generarCombinacionesDeParentesis(n);
+
+    cout << "Combinaciones válidas de paréntesis:" << endl;
+    for (const string& combinacion : combinaciones) {
+        cout << combinacion << endl;
+    }
+
+    return 0;
+}
