2cc6d54016f16901f9a4835434359256f6f1be5a
1329223_YasLAB <123830336+Yas1329223@users.noreply.github.com>
2023-09-11 22:34:45 -0600

    Update Ejercicio03.cpp

--- HojaDeTrabajo01/Ejercicio03.cpp
+++ HojaDeTrabajo01/Ejercicio03.cpp
@@ -7,6 +7,22 @@ bool Ejercicio03::isPowerOfTwo(int n)
     {
         return true;
     }
-    else if (n % 2 != 0 
-    
+    else if (n % 2 != 0 || n == 0)
+    {
+        return false;
+    }
+    return Potencia2 (n/2);
+}
+int main()
+{
+    int numero;
+    cout << "Ingrese un número: "; cin >> numero;
+
+    if (esPotenciaDeDos(numero)) {
+        cout << numero << " es una potencia de dos // true " << endl;
+    } else {
+        cout << numero << " no es una potencia de dos // false " << endl;
+    }
+
+    return 0;
 }
